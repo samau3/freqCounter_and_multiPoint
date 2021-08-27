409e39c3c8d4584e252223370948c0a5bf8e87a5
Dan Benson <dbenson3091@gmail.com>
2021-08-27 18:06:50 -0500

    fixed average-pair, checked for correct testing

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,35 @@
+"use strict";
 // add whatever parameters you deem necessary
-function averagePair() {
+
+/** Function accepts array and number, returns true if number matches average of an array pair, returns false if not
+ *
+ */
+
+//TODO:
+//1. Need to check pair in array. (2 values);
+//Set two pointer variables one at beginning, one at end
+//Check pointer values with loop.
+//2. Using a loop
+//Set a new variable based average pair of both current points
+//If average equals NUMBER return true
+//If average is less then add to pointer beginning
+//Else substract at pointer end
+//3. When out of loop return false;
+
+function averagePair(arr, num) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let average = arr[left] + arr[right] / 2;
+    if (average === num) {
+      return true;
+    } else if (average < num) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return false;
 }
